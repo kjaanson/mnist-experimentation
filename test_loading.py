3ca99228842f77473fb844c9526308acad45c3a9
Kaur Jaanson <kaur.jaanson@ttu.ee>
2017-10-11 00:00:08 +0300

    Code beautification

--- test_loading.py
+++ test_loading.py
@@ -7,5 +7,6 @@ from helpers import loading
 
 #laeb treening data-st laisalt tagastades (label, pilt) tupple-id
 training_gen = loading.read(path="data")
+label, pixels = next(training_gen)
 
-e = next(training_gen)
+loading.show(pixels)
